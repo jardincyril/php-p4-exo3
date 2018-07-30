e26af382fd75e667ea3c814e0bbbc37243a3d80c
cyril jardin <cyilroc60@hotmail.fr>
2018-07-30 09:46:34 +0200

    fonction

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>exo3</title>
+</head>
+<body>
+  <p>
+    <?php
+    function melting($string1, $string2){
+      $result = $string1. ' '. $string2;
+      return $result;
+    }
+    echo melting('hello','wolrd');
+    ?>
+  </p>
+</body>
+</html>
